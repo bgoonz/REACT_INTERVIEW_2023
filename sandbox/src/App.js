9849a25c11e78af3d7b6b65966f2a0423803d028
Bryan C Guner <bryan.guner@gmail.com>
2023-05-16 12:25:32 -0400

    grid component for displaying components

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import Grid from "./components/UI/Grid";
 import Spacer from "./components/UI/Spacer";
 import List from "./components/add-remove-list-items/List";
+import Counter from './components/count-between-one-and-five/Counter';
 import Parent from "./components/counter/Parent";
 import DisableButton from "./components/disable-button/DisableButton";
 import DisplayInput from "./components/display-input-text/DisplayInput";
@@ -9,37 +11,19 @@ import FetchComments from "./components/fetch-comments/FetchComments";
 import Sum from "./components/sum-of-numbers/Sum";
 import Toggle from "./components/toggle-show-hide/Toggle";
 import ParentComponent from "./components/update-parent-state/ParentComponent";
-import Counter from './components/count-between-one-and-five/Counter'
 import { CountContextProvider } from "./context/store";
 
-
 function App() {
+    const components = [ Counter, Sum, List, FetchComments, Users, Toggle, DisplayInput, DisableButton, ParentComponent, Counter]
   return (
     <div className="App">
       <h1>React Interview Challenges</h1>
+      <Grid components={components} />
       <Spacer />
       <CountContextProvider>
         <Parent />
       </CountContextProvider>
-      <Spacer />
-      <List />
-      <Spacer />
-      <FetchComments />
-      <Spacer />
-      <Users />
-      <Spacer />
-      <Toggle />
-      <Spacer />
-      <DisplayInput />
-      <Spacer />
-      <DisableButton />
-      <Spacer />
-      <ParentComponent />
-      <Spacer />
-      <Sum />
-      <Spacer />
-      <Counter />
-      <Spacer />
+     
     </div>
   );
 }
---
+++ sandbox/src/components/UI/Grid.js
@@ -0,0 +1,15 @@
+import classes from "./Grid.module.css";
+const Grid = ( { components } ) => {
+  return (
+    <div className={classes["grid-container"]}>
+      {components.map((Component, index) => (
+        <div className={classes["grid-item"]} key={index}>
+          <Component />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Grid;
+
